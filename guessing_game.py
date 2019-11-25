b98abb8757c679e4990b5a4fe08d53d2d64d8ce4
Aaron Smith <smitham50@gmail.com>
2019-11-25 09:44:01 -0500

    done

---
+++ guessing_game.py
@@ -0,0 +1,26 @@
+import random
+
+random_num = random.randint(1,10)
+
+game_over = False
+
+guess = input("Guess a number between 1 and 10 ")
+
+while not game_over:
+    guess = int(guess)
+    if random_num < guess:
+        guess = input("Too high, try again! ")
+    elif random_num > guess:
+        guess = input("Too low, try again! ")
+    else:
+        print(f"Correct! The number was {random_num}")
+        again = input("Do you want to play again? (y/n) ")
+        if again == "y":
+             guess = input("Guess a number between 1 and 10 ")
+        else:
+            game_over = True
+
+
+
+
+        
